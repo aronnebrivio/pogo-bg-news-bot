a5bd10fab9bc785567b30ad233dac433d1a39e48
Arone Brivio <aroblu94@gmail.com>
2019-03-25 21:52:32 +0100

    Test 2

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ def handle(msg):
     elif 'caption' in msg:
         txt = txt + msg['caption']
 
-    if msg['chat']['type'] in ['group', 'supergroup'] and len(msg['new_chat_members']) > 0:
+    if msg['chat']['type'] in ['group', 'supergroup'] and msg['new_chat_members'] and len(msg['new_chat_members']) > 0:
         newMembers = msg['new_chat_members']
         foundLeMe = False
         for member in newMembers:
@@ -29,7 +29,7 @@ def handle(msg):
             CHATS.append(msg['chat']['id'])
     if msg['chat']['type'] == 'channel' and is_allowed(msg) and txt != '':
         for chat in CHATS:
-            bot.forwardMessage(chat, chat_id, msg['message_id'])
+            bot.forwardMessage(chat, SOURCE, msg['message_id'])
 
 def is_allowed(msg):
     if msg['chat']['id'] == SOURCE:
@@ -48,8 +48,8 @@ BOT_ID = os.environ.get('BOT_ID')
 TOKEN = os.environ.get('BOT_TOKEN')
 PASSWORD = os.environ.get('ADMIN_PASSWORD')
 
-if TOKEN == '' or PASSWORD == '':
-    sys.exit('No TOKEN or PASSWORD in environment')
+if TOKEN == '' or PASSWORD == '' or BOT_ID == '':
+    sys.exit('No TOKEN, PASSWORD or BOT_ID in environment')
 
 with open('chats.json', 'r') as f:
     CHATS = f.read().split(',')
